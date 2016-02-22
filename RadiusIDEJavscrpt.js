c5026d1baef81829416352540a438fb68b29b670
chrishernan <christian.hernandez.2015@gmail.com>
2016-02-22 14:49:37 -0800

    organization and jsPanel update
    I broke up the messy single html file i had to 3 separate files that
    will represent the javascript, css, and html files. It also has the
    jsPanel functionality implemented into 3 panels that I set up as not
    resizable just to keep things tidy. It can all be changed easily on the
    .js file.

---
+++ RadiusIDEJavscrpt.js
@@ -0,0 +1,24 @@
+jQuery.jsPanel( {
+	title: "Control Pane",
+	position: "top left",
+	resizable: "disabled",
+	size: {width:320 , height:640},
+	theme: "light"
+});
+jQuery.jsPanel( {
+	title:"Editing Pane",
+	position: "top right",
+	resizable: "disable",
+	size: {width:950,height:360},
+	theme: "light"
+
+});
+
+jQuery.jsPanel( {
+	title: "Console Pane",
+	position: "bottom right",
+	resizable: "disable",
+	size: {width:950,height:250},
+	theme: "black"
+	
+});
