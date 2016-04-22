e72f7ad703c87cec5263f4992c17e455f5f86674
chrishernan <christian.hernandez.2015@gmail.com>
2016-04-22 06:48:54 -0700

    Added a picture that should represent the folders of every assignment and also added the assignments div. when you click the assignments button it hides the control panel and shows the assignments panel.

--- Radius2U/js/RadiusPanes.js
+++ Radius2U/js/RadiusPanes.js
@@ -10,6 +10,76 @@ $(function () {
     });
 
 
+    //Changes the visibility of the assignments pane by a toggle button
+    $('#b-assignments').click(function() {
+        var table;
+        var row;
+        var cell1;
+        var cell2;
+        /*document.getElementById("assignment-panel").style.visibility= 'visible';
+        if($("#assignment-panel").is(':hidden')){
+            alert("in if");
+            document.getElementById("assignment-panel").style.visibility= 'visible';
+        }
+        else {
+            alert("in else");
+            //document.getElementById("assignment-panel").style.visibility = 'hidden';
+
+        }
+        alert("end");*/
+        if(document.getElementById("assignment-panel").style.display==='block') {
+            document.getElementById("assignment-panel").style.display='none';
+            document.getElementById("control-panel").style.display='block';
+
+        }
+        else {
+            document.getElementById("assignment-panel").style.display='block';
+            document.getElementById("control-panel").style.display='none';
+        }
+
+        var data = [{"name":"Assignment 1","due_date":"08/16/17"},{"name":"Assignment 2","due_date":"08/24/17"},{"name":"Assignment 3","due_date":"09/03/17"},{"name":"Assignment 4","due_date":"09/25/17"}];
+        var counter = 0;
+
+        table = document.getElementById("dataHolder");
+
+
+        for(var key in data) {
+            row = table.insertRow(counter);
+            cell1 = row.insertCell(0);
+            cell2 = row.insertCell(1);
+            cell1.innerHTML = "<img src='/Users/christianhernandez/WebstormProjects/RadiusIDE_1/Radius2U/folder_icon.png'>";
+            cell2.innerHTML = data[key].name;
+            counter++;
+        }
+
+        /*
+        table = $('.dataHolder').DataTable({
+             rows: [
+                {'name':'assignment 1'},
+                {'name':'assignment 2'},
+                {'name':'assignment 3'},
+                {'name':'assignment 4'},
+            ]
+        });
+        var data = {"assignments": [{"name":"Assignment 1"},{"name":"Assignment 2"},{"name":"Assignment 3"},{"name":"Assignment 4"}]};
+    */
+/*
+        table.row.add([
+            'Assignment 1',
+            'Assignment 2' ,
+            'Assignment 3',
+            'Assignment 4'
+        ]).draw(false);*/
+
+
+
+    })
+
+    $("#dataHolder").click(function () {
+        alert("Due Date: TBA");
+
+    })
+
     //All buttons in Toolbar
     $("#b-login").button({icons: {primary: "ui-icon-arrowthickstop-1-e"},});
     $("#b-new").button({icons: {primary: "ui-icon-document"}});
@@ -17,8 +87,9 @@ $(function () {
     $("#b-trash").button({icons: {primary: "ui-icon-trash",},});
     $("#b-lock").button({icons: {primary: "ui-icon-locked",},});
     $("#b-settings").button({icons: {primary: "ui-icon-gear",},});
-    $("#b-run").button({icons: {primary: "ui-icon-play",},});
+    $("#b-run").button({icons: {primary: "u.i-icon-play",},});
     $("#b-stop").button({icons: {primary: "ui-icon-stop",},});
+    $("#b-assignments").button({icons:{primary: "ui-icon-folder-collapsed",},});
 
     function show(id) {
         $(id).style.display = 'block';
@@ -71,6 +142,7 @@ $(function () {
     }
 
     function onUp() {
+
         if (st != "undefined") { // prevent buttons from calling this         //snap the left 
             if (st.offset().left <= 150) {
                 setBounds(id, 0, 0, 25, 100);
