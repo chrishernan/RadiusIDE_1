6c81905ce081533a5e612fc72153ea21d5d17b13
chrishernan <christian.hernandez.2015@gmail.com>
2016-05-15 23:25:35 -0700

    The update functions is almost complete and you can change the values of certain properties of assignments whenever you want.

--- Radius2U/js/UploadAssignment.js
+++ Radius2U/js/UploadAssignment.js
@@ -5,25 +5,76 @@ $(function() {
    
     var url = "https://radius-ide.firebaseio.com/Teachers/Frost/Students/"
     var ref = new Firebase(url)
+    var name;
+    var dueDate;
+    var starterCode;
+
 
     $("#upload-assignments").click(function () {
         //add a floating div that prompts the user to input the starter code,
         //due date, name
         //$("#upload-assignments-prompt").style.display = 'block';
 
-        ref.once("value",function(snapshot) {
-            snapshot.forEach(function(childSnapshot) {
-                var child = childSnapshot.key();
-               // var student = child.val();
+        document.getElementById("upload-form").style.display = "block";
 
-                alert(child);
-            });
-        })
+
 
 
     })
 
+
+    //user clicks on submit button
+    $("#upload-form-submit").click(function() {
+
+
+        name = document.getElementById("Starter-Code-Form1").value;
+        dueDate = document.getElementById("Starter-Code-Form2").value;
+        starterCode = document.getElementById("Starter-Code-Form3").value;
+        Description = document.getElementById("Starter-Code-Form4").value;
+
+        var tempFirebase;
+
+        ref.once("value",function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                var studentData = childSnapshot.val();
+                // var student = child.val();
+
+                alert(childSnapshot.key());
+                tempFirebase = new Firebase("https://radius-ide.firebaseio.com/Teachers/Frost/Students/"+childSnapshot.key()+"/Assignments/"+name);
+
+                // Same as the previous example, except we will also display an alert
+                // message when the data has finished synchronizing.
+                var onComplete = function(error) {
+                    if (error) {
+                        console.log('Synchronization failed');
+                    } else {
+                        console.log('Synchronization succeeded');
+                    }
+                };
+
+
+                tempFirebase.update({Descriptions:Description,DueDate:dueDate,Last_Saved_State:'',Starter_Code:starterCode},onComplete());
+
+
+
+            });
+
+        })
+
+
+
+
+    })
+
+
+    $("#upload-close").click( function() {
+
+        document.getElementById("upload-form").style.display = "none";
+
+    })
     //when the teacher fills in and submits the assignment fields we make the div dissapear again.
+
+
 
 
     //update assignments
@@ -34,4 +85,6 @@ $(function() {
 
 
 
-});
+})
+
+
